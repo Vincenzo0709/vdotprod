9a0aabe9a726123b9f79c1003caf611d38410d0a
Vincenzo0709 <vmerola99@gmail.com>
2025-04-04 17:34:28 +0200

    [FETCH] Update fetch_sources.sh and add Vitis implementation

--- scripts/fetch_sources.sh
+++ scripts/fetch_sources.sh
@@ -16,33 +16,46 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 GIT_URL=https://github.com/Vincenzo0709/vdotprod.git
-GIT_TAG=main
+GIT_TAG=v1.0
 CLONE_DIR=vdotprod
 COMP_NAME=${CLONE_DIR}
 
-# Display help
+# Displays help
 Help()
 {
-   echo
-   echo "This script downloads from Git or directly builds Vitis HLS vdotprod accelerator sources"
-   echo "and flattens rtl files, Vivado ip directory and ip .zip into a build/ directory."
-   echo
-   echo "Syntax: fetch_source.sh [--option]"
-   echo
-   echo "options:"
-   echo "-h | --help            Prints help"
-   echo
+   cat << EOF
+
+This script downloads from Git or directly builds Vitis HLS vdotprod accelerator sources
+and flattens rtl files, Vivado ip directory and ip .zip into a build/ directory.
+
+Syntax: fetch_sources.sh [--option]
+
+options:
+    -c | --clean            Cleans output artifacts
+    -h | --help             Prints help
+
+EOF
+
 }
 
-GitFlatten()
-{
+# Cleans output artifacts
+Clean() {
+
+    cd $(dirname "$0")
+    cd ../hw/
+    make clean
+    rm -fr build/
+
+}
+
+GitFlatten() {
 
     printf "\n${GREEN}[FETCH_SOURCES] Fetching with git${NC}\n"
 
     # Creating build/ dir
     rm -rf build/
     mkdir -p build/rtl build/ip
-    BUILD="`pwd -P`/build"
+    BUILD=$(pwd -P)/build
 
     # Cloning repo
     printf "\n${YELLOW}[FETCH_SOURCES] Cloning source repository${NC}\n"
@@ -54,12 +67,12 @@ GitFlatten()
 
     cd ${CLONE_DIR}/hw
     # Synthesizing and copying verilog rtl files
-    printf "\n${YELLOW}[FETCH_SOURCES] Starting synthesis${NC}\n"
+    printf "\n${YELLOW}[FETCH_SOURCES] Synthesizing and copying files${NC}\n"
     make syn
     cp -r ${COMP_NAME}/hls/syn/verilog/* ${BUILD}/rtl
 
     # Packaging and copying Vivado ip files and ip .zip
-    printf "\n${YELLOW}[FETCH_SOURCES] Starting packaging${NC}\n"
+    printf "\n${YELLOW}[FETCH_SOURCES] Packaging and copying files${NC}\n"
     make package
     cp ${COMP_NAME}_hls.zip ${BUILD}/
     cp -r ${COMP_NAME}/hls/impl/ip/* ${BUILD}/ip
@@ -80,20 +93,19 @@ Flatten() {
     cd hw
     rm -rf build/
     mkdir -p build/rtl build/ip
-    BUILD="`pwd -P`/build"
+    BUILD=$(pwd -P)/build
 
     # Clone Bender (future development)
     # printf "\n${YELLOW}[FETCH_SOURCES] Download Bender${NC}\n"
     # curl --proto '=https' --tlsv1.2 https://pulp-platform.github.io/bender/init -sSf | sh
 
-    # Copy all RTL files into rtl dir
-    printf "\n${YELLOW}[FETCH_SOURCES] Copying all sources into rtl${NC}\n"
-
     # Synthesizing and copying verilog rtl files
+    printf "\n${YELLOW}[FETCH_SOURCES] Synthesizing and copying files${NC}\n"
     make syn
     cp -r ${COMP_NAME}/hls/syn/verilog/* ${BUILD}/rtl
 
     # Packaging and copying Vivado ip files and ip .zip
+    printf "\n${YELLOW}[FETCH_SOURCES] Packaging and copying files${NC}\n"
     make package
     mv ${COMP_NAME}_hls.zip ${BUILD}
     cp -r ${COMP_NAME}/hls/impl/ip/* ${BUILD}/ip
@@ -103,14 +115,19 @@ Flatten() {
 
 }
 
-OPTS=$(getopt -o h --long help -n 'fetch_sources.sh' -- "$@")
+OPTS=$(getopt -o hc --long help,clean -n 'fetch_sources.sh' -- "$@")
 eval set -- "$OPTS"
 
 if [ "$OPTS" != " --" ]; then
     while true; do
         case "$1" in
+            -c | --clean)
+                # Cleans output artifactis
+                Clean
+                exit 0
+                ;;
             -h | --help) 
-                # Print help
+                # Displays help
                 Help
                 exit 0
                 ;;
@@ -131,10 +148,10 @@ fi
 
 # Moving to right directory (if you cloned the whole repository instead of using only fetch_sources.sh)
 cd "$(dirname "$0")" ; printf "\n${GREEN}[FETCH_SOURCES] Starting from directory `pwd -P`${NC}\n"
-WORK_DIR=`pwd -P`
-if [ "$(basename `pwd -P`)" == "scripts" ]; then
+WORK_DIR=$(pwd -P)
+if [ "$(basename $(pwd -P))" == "scripts" ]; then
     cd .. ; printf "\n${GREEN}[FETCH_SOURCES] Moving to directory `pwd -P`${NC}\n"
-    WORK_DIR=`pwd -P`
+    WORK_DIR=$(pwd -P)
     Flatten
 else
     GitFlatten
